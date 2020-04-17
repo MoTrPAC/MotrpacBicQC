68692dea039af5f93f699311295966c497b41ded
David JM <biodavidjm@gmail.com>
2020-04-17 13:02:36 -0700

    Add package data structure
    - Create structure to add data to the package
    - Add bic tissue code names as first example data
    #1

---
+++ inst/extdata/generateRdata.R
@@ -0,0 +1,6 @@
+# R script to generate R data objects used by the library
+
+# BIC tissue codes
+bic_animal_tissue_code <- read.delim("inst/extdata/bic_animal_tissue_code.txt", stringsAsFactors = FALSE)
+save(bic_animal_tissue_code, file = "data/bic_animal_tissue_code.Rdata")
+
