f92f897df598d70d078bc6883214473768184d76
David JM <biodavidjm@gmail.com>
2020-04-17 14:19:42 -0700

    Fix Rdata to RData
    - `R CMD check` requires that the Rdata files being `RData`
    #1

--- inst/extdata/generateRdata.R
+++ inst/extdata/generateRdata.R
@@ -2,5 +2,5 @@
 
 # BIC tissue codes
 bic_animal_tissue_code <- read.delim("inst/extdata/bic_animal_tissue_code.txt", stringsAsFactors = FALSE)
-save(bic_animal_tissue_code, file = "data/bic_animal_tissue_code.Rdata")
+save(bic_animal_tissue_code, file = "data/bic_animal_tissue_code.RData")
 
