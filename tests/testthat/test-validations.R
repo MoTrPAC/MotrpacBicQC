bd81d1a7189c3ef7b127faf8a667c43f9b7ce12f
David JM <biodavidjm@gmail.com>
2022-09-17 17:37:48 -0700

    feat: support `BICRESULTS` folder; update tests

--- tests/testthat/test-validations.R
+++ tests/testthat/test-validations.R
@@ -5,12 +5,20 @@ test_that("All validations works", {
   y <- "MORESTUFF/PASS1A-06/T31/IONPNEG/BATCH1_20190909/PROCESSED_20200205/EVENMOREFOLDERS/"
   z <- "PASS1B-06/T100/IONPNEG/BATCH1_20190909/PROCESSED_20200205"
   b <- "PASS1A-06/T31/IONPNEG/BATCH1_2019090990/PROCESSED_20200205"
+  h <- "HUMAN/T10/IONPNEG/BATCH1_20190909/RESULTS_20220101"
+  j <- "HUMAN/T10/IONPNEG/BATCH1_20190909/BICRESULTS_20220101"
+  k <- "HUMAN/T10/IONPNEG/BATCH1_20190909/TOPRESULTS_20220101"
   expect_equal(validate_processFolder(x), "PROCESSED_20200205")
   expect_equal(validate_processFolder(y), "PROCESSED_20200205")
   expect_equal(validate_assay(x), "IONPNEG")
   expect_equal(validate_phase(x), "PASS1A-06")
   expect_equal(validate_phase(y), "PASS1A-06")
   expect_equal(validate_tissue(x), "T31")
+  expect_equal(validate_batch(h), "HUMAN/T10/IONPNEG/BATCH1_20190909/")
+  expect_equal(validate_batch(j), "HUMAN/T10/IONPNEG/BATCH1_20190909/")
+  expect_equal(validate_processFolder(h), "RESULTS_20220101")
+  expect_equal(validate_processFolder(j), "BICRESULTS_20220101")
+  expect_no_match(validate_processFolder(k), "TOPRESULTS_20220101")
   expect_error(validate_tissue(z))
   expect_equal(validate_batch(x), "PASS1A-06/T31/IONPNEG/BATCH1_20190909/")
   expect_error(validate_batch(b))
